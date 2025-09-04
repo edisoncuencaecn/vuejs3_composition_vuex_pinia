9a1a936ccbcbfb6198ebad9076895801d216ca83
Edison Cuenca <ingfcuenca@gmail.com>
2025-09-04 14:11:00 -0400

    fixed issue with string.charAt(0), replaced with string.[0], created child component Display

--- calculator/src/components/CalculatorOld.vue
+++ calculator/src/components/CalculatorOld.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="calculator">
+    <!--div v-bind:result="result" id="display">{{ 0.00 || result }}</div-->
     <Display v-bind:result="result" />
     <div id="keys">
         <button v-on:click="clearDisplay()" class="operator-button">AC</button>
@@ -33,46 +34,53 @@
    import Display from './Display.vue'
    import {ref} from 'vue';
    let result = ref('0.00');
+   let totalized = ref(false); // to avoid charAt() error evaluation after eval() used by button '=', resolved using string[0] instead string.charAt(0)
    
    //`` backticks embed expressions (variables, function calls, arithmetic operations, etc.) directly within a string.
    // removes left first '0' or '.' and embed pressed key to main result string
    const toDisplay = (key) => {
-     result.value =  result.value[0] === "0" ? result.value.charAt(1,result.value.length) : result.value;
-     result.value =  result.value[0] === "." ? result.value.charAt(1,result.value.length) : result.value;
-     result.value = `${result.value}${key}`;  
+      if (totalized !== true){
+        result.value =  result.value.charAt(0) === "0" ? result.value.charAt(1,result.value.length) : result.value;
+        result.value =  result.value.charAt(0) === "." ? result.value.charAt(1,result.value.length) : result.value;
+        result.value = `${result.value}${key}`; 
+      } else{
+        result.value = `${result.value}${key}`; 
+      }
    };
    
    const signDisplay = () => {
-     // removes '-' if it's already there, otherwise add it
-     try {
-       result.value =  result.value[0] === "-" ? `${result.value.slice(1,result.value.length)}` : `-${result.value}`;
-     } catch (error) {
-       result.value = "error";
-    }
+      // removes '-' if it's already there, otherwise add it
+      try {
+        result.value =  result.value.charAt(0) === "-" ? `${result.value.slice(1,result.value.length)}` : `-${result.value}`;
+      } catch (error) {
+        result.value = "error";
+      }
    };
 
    const clearDisplay = () => {
-     result.value = '0';
+      result.value = '0';
+      totalized = false;
    };
 
    const help = () => {
-     alert('type percentNumber followed by "%" then "x" followed by the number to get % from, finally "="');
+    alert('type percentNumber followed by "%" then "x" followed by the number to get % from, finally "="');
    }
 
    const percentToDisplay = () => {
-     try {
-       result.value = `${result.value}/100`;   
-     } catch {
-       result.value = "error";
-     }
+      try {
+        result.value = `${result.value}/100`;
+      } catch {
+        result.value = "error";
+      }
    };
    
   const calculate = () => {
-    try {
-      result.value = eval(`${result.value}`); 
-    } catch (error) {
-      result.value = "error";
-    }
+      try {
+        result.value = eval(`${result.value}`); 
+      } catch (error) {
+        result.value = "error";
+      }
+      totalized = true;
    };
 
 </script>
---
+++ calculator/src/components/Display.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="display">{{ 0.00 || result }}</div>
+</template>
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps({
+  result: {
+    type: String,
+    required: true,
+  },
+})
+</script>
