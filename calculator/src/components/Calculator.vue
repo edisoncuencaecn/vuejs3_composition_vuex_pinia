b2c562ef4feb73f2ce38ec3da1f19908be47c432
Edison Cuenca <ingfcuenca@gmail.com>
2025-09-14 10:23:59 -0400

    Fixed issue with 5 % = - 5 been 5 instead -4.95

--- calculator/src/components/Calculator.vue
+++ calculator/src/components/Calculator.vue
@@ -56,6 +56,7 @@ Also passing computed data via emit to parent component App.vue-->
     // set focus ends
 
    let result = ref('0.00');
+   let resultEvalLength = ref(0);
    let resultPrevious = ref('0.00');
    let keyPrevious = ref('0');
    let numDigits = ref(0); 
@@ -66,6 +67,11 @@ Also passing computed data via emit to parent component App.vue-->
    const mathOperators = ref(['+','-','/','*','%']); 
    const shift = 'Shift'; 
    let isKeyboard = ref(false); 
+
+    const getResutLength = () => {
+      console.log('length:',result.value?.length ?? 0)
+       return result.value?.length ?? 0
+    }
 
    const switchToKeyboard = () => {
      isKeyboard.value = !isKeyboard.value;
@@ -80,8 +86,8 @@ Also passing computed data via emit to parent component App.vue-->
         if (result.value === 'error' || result.value === 'Infinity' || result.value === '-Infinity'){
          result.value ='';
         };
-       result.value =  result.value[0] === "0" ? result.value.charAt(1,result.value?.length ?? 0) : `${result.value}`; // Optional chaining data?.length ?? 0 (ES2020)
-       result.value =  result.value[0] === "." ? result.value.charAt(1,result.value?.length ?? 0) : `${result.value}`;
+       result.value =  result.value[0] === "0" ? result.value.charAt(1,getResutLength()) : `${result.value}`; // Optional chaining data?.length ?? 0 (ES2020)
+       result.value =  result.value[0] === "." ? result.value.charAt(1,getResutLength()) : `${result.value}`;
        // if keyboard key is a n operations element, it means it won't be added to the string nor diplay it
        if (isKeyForOperation(event.key)){
          switch (event.key) {
@@ -103,16 +109,21 @@ Also passing computed data via emit to parent component App.vue-->
       }else{
         // if first input is '0' and the second is '.' then accepts it because it's a decimal number to be input
         result.value =  !mathOperatorExist() && keyPrevious.value == "0" && event.key === "." ?  `${" 0."}` : `${result.value}`; // adding condition && there's not already a operator
-        resultPrevious.value = result.value ? result.value : `${result.value}`;
+        //resultPrevious.value = result.value ? result.value : `${result.value}`;
         result.value = `${result.value}${event.key}`; 
         keyPrevious.value = event.key;
-        // removes first character when it's an operator  
-        result.value =  isKeyForOperators(result.value[0]) && (result.value?.length ?? 0) === 1 ? "0.00" : result.value;
+        if (resultEvalLength.value > 1){ // its already evalatuaded then result.value.length > 0, do not check: BIG ISSUE with decimals (getResutLength()) won't work 0.05 says length=1
+          result.value = `${resultPrevious.value}${event.key}`; // issue removed the '-'
+          resultPrevious.value = result.value ? result.value : `${result.value}`;
+        } else {
+          // removes first character when it's an operator  
+          result.value =  isKeyForOperators(result.value[0]) && (getResutLength()) === 1 ? "0.00" : `${result.value}`;//result.value;
+        }
         // removes second operator if it's input in a row
-        result.value =  isKeyForOperators(result.value[(result.value?.length ?? 0)-1]) && isKeyForOperators(result.value[(result.value?.length ?? 0)-2]) ? result.value.slice(0,((result.value?.length ?? 0)-1)) : result.value; //here
+        result.value =  isKeyForOperators(result.value[(getResutLength())-1]) && isKeyForOperators(result.value[(getResutLength())-2]) ? result.value.slice(0,((getResutLength())-1)) : `${result.value}`;//result.value; //here
         // removes second operator period if it's input 
-        result.value =  isKeyForOperators(result.value[(result.value?.length ?? 0)-1]) && isPeriodKeyReplicated('.') ? result.value.slice(0,((result.value?.length ?? 0)-1)) : result.value; //here
-        numDigits.value = result.value?.length ?? 0; // Optional chaining data?.length ?? 0 (ES2020)
+        result.value =  isKeyForOperators(result.value[(getResutLength())-1]) && isPeriodKeyReplicated('.') ? result.value.slice(0,((getResutLength())-1)) : `${result.value}`;//result.value; //here
+        numDigits.value = getResutLength(); // Optional chaining data?.length ?? 0 (ES2020)
         // To remove 'Shift' key from keyboardEvent 'Shift =' & 'Shift *'
         result.value =  result.value.indexOf(shift) !== -1 ? result.value.slice(0, (result.value.indexOf(shift)), "+") : result.value;
         if (numDigits.value > 21){ 
@@ -161,7 +172,8 @@ Also passing computed data via emit to parent component App.vue-->
     const hasAllowedKeys = (key) => {
        return allowedKeys.value.includes(key);
     };
-  
+    // vpi.toPrecision(5)
+
     // ends keyboard
 
    //`` backticks embed expressions (variables, function calls, arithmetic operations, etc.) directly within a string.
@@ -173,21 +185,27 @@ Also passing computed data via emit to parent component App.vue-->
      if (result.value === 'error' || result.value === 'Infinity' || result.value === '-Infinity'){
        result.value ='';
      };
-     result.value =  result.value[0] === "0" ? result.value.charAt(1,result.value?.length ?? 0) : `${result.value}`;
-     result.value =  result.value[0] === "." ? result.value.charAt(1,result.value?.length ?? 0) : `${result.value}`;
+     result.value =  result.value[0] === "0" ? result.value.charAt(1,getResutLength()) : `${result.value}`;
+     result.value =  result.value[0] === "." ? result.value.charAt(1,getResutLength()) : `${result.value}`;
      // if first input is '0' and the second is '.' then accepts it because it's a decimal number to be input 
      result.value =  !mathOperatorExist() && keyPrevious.value == "0" && key === "." ?  `${" 0."}` : `${result.value}`; // adding condition && there's not already a operator
-     resultPrevious.value = result.value ? result.value : `${result.value}`;
-     result.value = `${result.value}${key}`;  
+     //resultPrevious.value = result.value ? result.value : `${result.value}`; // NaN
+     result.value = `${result.value}${key}`;  // issue decimal number previously displayed + operator becaomes NaN
      keyPrevious.value = key;
      // removes first character when it's an operator  
-     result.value =  isKeyForOperators(result.value[0]) && (result.value?.length ?? 0) === 1 ? "0.00" : result.value;
+     if (resultEvalLength.value > 1){ // its already evalatuaded then result.value.length > 0, do not check: BIG ISSUE with decimals (getResutLength()) won't work 0.05 says length=1
+       //console.log('>length:',resultEvalLength.value,result.value[resultEvalLength.value], 'result.value:',result.value, Number(result.value).toPrecision(11), 'resultPrevious:',resultPrevious.value,'numberDigits:',numDigits.value);
+       result.value = `${resultPrevious.value}${key}`; // issue removed the '-'
+       resultPrevious.value = result.value ? result.value : `${result.value}`;
+     } else {
+        result.value =  isKeyForOperators(result.value[0]) && ((getResutLength()) === 1) ? "0.00" : `${result.value}`;//result.value;
+     }
      // removes second operator if it's input in a row
-     result.value =  isKeyForOperators(result.value[(result.value?.length ?? 0)-1]) && isKeyForOperators(result.value[(result.value?.length ?? 0)-2]) ? result.value.slice(0,((result.value?.length ?? 0)-1)) : result.value; //here
+     result.value =  isKeyForOperators(result.value[(getResutLength())-1]) && isKeyForOperators(result.value[(getResutLength())-2]) ? result.value.slice(0,((getResutLength())-1)) : `${result.value}`;//result.value; //here
      // removes second operator period if it's input 
-     result.value =  isKeyForOperators(result.value[(result.value?.length ?? 0)-1]) && isPeriodKeyReplicated('.') ? result.value.slice(0,((result.value?.length ?? 0)-1)) : result.value; //here
+     result.value =  isKeyForOperators(result.value[(getResutLength())-1]) && isPeriodKeyReplicated('.') ? result.value.slice(0,((getResutLength())-1)) : `${result.value}`;//result.value; //here
 
-     numDigits.value = result.value?.length ?? 0; // Optional chaining data?.length ?? 0 (ES2020)
+     numDigits.value = getResutLength(); // Optional chaining data?.length ?? 0 (ES2020)
      if (numDigits.value > 21){ 
        result.value = resultPrevious.value; // just won't add the new digit nor operator    
      };
@@ -201,7 +219,7 @@ Also passing computed data via emit to parent component App.vue-->
          return result.value ='0.00';
      };
      resultPrevious.value = result.value ? result.value : `${result.value}`;
-     if  (isKeyForOperators(result.value[(result.value?.length ?? 0)-1])){
+     if  (isKeyForOperators(result.value[(getResutLength())-1])){
         // if last character is an operator, don't do anything because the formula is pending
         result.value = resultPrevious.value; // just won't add the new digit nor operator  
         return result.value;
@@ -211,7 +229,7 @@ Also passing computed data via emit to parent component App.vue-->
        resultPrevious.value = result.value ? result.value : `${result.value}`;
        result.value = `${eval(result.value)}`; // evaluates if +/- is input in a pending formula
        result.value =  result.value <= 0 ? `${result.value *(-1)}` : `-${result.value}`;
-       numDigits.value = result.value?.length ?? 0; // Optional chaining data?.length ?? 0 (ES2020)
+       numDigits.value = getResutLength(); // Optional chaining data?.length ?? 0 (ES2020)
        if (numDigits.value > 21){ 
         result.value = resultPrevious.value; // just won't add the new digit nor operator    
        };
@@ -228,21 +246,24 @@ Also passing computed data via emit to parent component App.vue-->
      resultPrevious.value = '0.00';
      numDigits.value = 0;
      keyPrevious.value = '0';
+     resultEvalLength.value = 0;
    };
 
 
    const percentToDisplay = () => {
+     if (result.value === 'error' || result.value === 'Infinity' || result.value === '-Infinity'){
+         return result.value ='0.00';
+     };
      resultPrevious.value = result.value ? result.value : `${result.value}`;
      if (isKeyboard){
         setFocus(); // sets automatic keyboard keys focus
      };
-   
      try {
        // removes first character when it's an operator  
        if (result.value > 0) {
-         console.log((result.value));
          result.value = `${result.value}/100`;  
-         numDigits.value = result.value?.length ?? 0; // Optional chaining data?.length ?? 0 (ES2020)
+         //keyPrevious.value = '%';
+         numDigits.value = getResutLength(); // Optional chaining data?.length ?? 0 (ES2020)
          if (numDigits.value > 21){ 
            result.value = resultPrevious.value; // just won't add the new digit nor operator 
          } 
@@ -257,19 +278,22 @@ Also passing computed data via emit to parent component App.vue-->
       setFocus(); // sets automatic keyboard keys focus
     };
     resultPrevious.value = result.value ? result.value : `${result.value}`;
-    if  (isKeyForOperators(result.value[(result.value?.length ?? 0)-1])){
+    if  (isKeyForOperators(result.value[(getResutLength())-1])){
         // if last character is an operator, don't do anything because the formula is pending
     }else{
        try {
-        //result.value = `${eval(result.value)}`;
-        result.value = `${parseFloat(eval(result.value).toFixed(11))}`;
+        result.value = `${eval(result.value)}`;
+        //result.value = `${parseFloat(eval(result.value).toFixed(11))}`;
          if (numDigits > 21){
            result.value = resultPrevious.value; // just won't add the new digit nor operator 
          } 
+         resultEvalLength.value = getResutLength()
+         resultPrevious.value = result.value ? result.value : `${result.value}`;
        } catch (error) {
          result.value = "error";
        }
     };
+     //console.log('0.05'?.length,',length:',getResutLength(),result.value[`${result.value}`.length], `${result.value}`, Number(result.value).toPrecision(11), 'resultPrevious:',resultPrevious.value);
    };
 
 </script>
